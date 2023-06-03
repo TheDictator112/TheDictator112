492488150d412fb816baea731c7049be81deb9e2
TheDictator112 <133062227+TheDictator112@users.noreply.github.com>
2023-06-03 18:45:35 -0400

    README.cc
    Readme.htm

---
+++ README.cc
@@ -0,0 +1,88 @@
+Configuration instructions
+
+Installation instructions
+
+Operating instructions
+
+A file manifest (a list of files in the directory or archive)
+
+Copyright and licensing information
+
+Contact information for the distributor or author
+
+A list of known bugs[3]
+
+Troubleshooting instructions[3]
+
+Credits and acknowledgments
+
+A changelog (usually aimed at fellow programmers)
+
+A news section (usually aimed at end users)
+
+History
+
+Learn more
+
+This section needs expansion. You can help by adding to it. (February 2015)
+
+It is unclear when the convention of including a README file began, but examples dating to the mid-1970s have been found.[4][5][6][7][8][9][2][better source needed] Early Macintosh system software installed a Read Me on the Startup Disk, and README files commonly accompanied third-party software.
+
+In particular, there is a long history of free software and open-source software including a README file; the GNU Coding Standards encourage including one to provide "a general overview of the package".[10]
+
+Since the advent of the web as a de facto standard platform for software distribution, many software packages have moved (or occasionally, copied) some of the above ancillary files and pieces of information to a website or wiki, sometimes including the README itself, or sometimes leaving behind only a brief README file without all of the information required by a new user of the software.
+
+The popular source code hosting website GitHub strongly encourages the creation of a README file – if one exists in the main (top-level) directory of a repository, it is automatically presented on the repository's front page. In addition to plain text, various other formats and file extensions are also supported,[11] and HTML conversion takes extensions into account – in particular a README.md is treated as GitHub Flavored Markdown.
+
+
+The expression "readme file" is also sometimes used generically, for other files with a similar purpose.[citation needed] For example, the source-code distributions of many free software packages (especially those following the Gnits Standards or those produced with GNU Autotools) include a standard set of readme files:
+
+README	
+General information
+AUTHORS	
+Credits
+THANKS	
+Acknowledgments
+
+CHANGELOG	
+A detailed changelog, intended for programmers
+A basic changelog, intended for users
+
+NEWS	
+
+INSTALL	
+Installation instructions
+
+COPYING / LICENSE	
+Copyright and licensing information
+
+BUGS	
+Known bugs and instructions on reporting new ones
+
+CONTRIBUTING / HACKING	
+Guide for prospective contributors to the project
+
+Also commonly distributed with software packages are an FAQ file and a TODO file, which lists planned improvements.
+
+See also
+
+Edit
+
+FILE_ID.DIZ
+
+DESCRIPT.ION
+
+.nfo
+
+man page
+
+- 👋 Hi, I’m @TheDictator112
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+TheDictator112/TheDictator112 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
